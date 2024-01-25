501b42999448a08726457ed3052400adb8357c6e
tnkfactory <tech@tnkfactory.com>
2024-01-25 14:05:37 +0900

    ios 라이브러리 최신화

---
+++ ios/TnkRwdSdk2.xcframework/ios-arm64/TnkRwdSdk2.framework/Headers/TnkRwdSdk2.h
@@ -0,0 +1,18 @@
+//
+//  TnkRwdSdk2.h
+//  TnkRwdSdk2
+//
+//  Created by  김동혁 on 2022/12/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TnkRwdSdk2.
+FOUNDATION_EXPORT double TnkRwdSdk2VersionNumber;
+
+//! Project version string for TnkRwdSdk2.
+FOUNDATION_EXPORT const unsigned char TnkRwdSdk2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TnkRwdSdk2/PublicHeader.h>
+
+
